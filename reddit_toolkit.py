e98c125f74c67d687807da0a9106a92c3305e74f
rynmccrmck <ryanjmccormack@gmail.com>
2023-09-30 13:59:20 -0400

    update keys

--- reddit_toolkit.py
+++ reddit_toolkit.py
@@ -1,9 +1,8 @@
 from abc import ABC
 from superagi.tools.base_tool import BaseToolkit, BaseTool, ToolConfiguration
-from typing import Type, List
+from typing import List
 from reddit_read_tool import RedditReadTool
 from reddit_write_tool import RedditWriteTool
-from superagi.types.key_type import ToolConfigKeyType
 
 class TwitterToolkit(BaseToolkit, ABC):
     name: str = "Reddit Toolkit"
@@ -13,8 +12,4 @@ class TwitterToolkit(BaseToolkit, ABC):
         return [RedditReadTool(), RedditWriteTool()]
 
     def get_env_keys(self) -> List[ToolConfiguration]:
-        return [
-            ToolConfiguration(key="REDDIT_CLIENT_ID", key_type=ToolConfigKeyType.STRING, is_required= True, is_secret = True),
-            ToolConfiguration(key="REDDIT_CLIENT_SECRET", key_type=ToolConfigKeyType.STRING, is_required=True, is_secret= True),
-            ToolConfiguration(key="REDDIT_CLIENT_USERAGENT", key_type=ToolConfigKeyType.STRING, is_required=True, is_secret= FALSE)
-        ]
+        return ["REDDIT_CLIENT_ID", "REDDIT_CLIENT_SECRET", "REDDIT_CLIENT_USERAGENT"]
